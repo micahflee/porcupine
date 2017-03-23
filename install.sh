928759d3e5b3455bbbee43222b9540cf483cb73d
Micah Lee <micah@micahflee.com>
2017-03-23 22:29:11 +0100

    Add Subgraph OS support, including a porcupine oz profile

--- install.sh
+++ install.sh
@@ -5,5 +5,21 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-sudo cp -r usr/* /usr/
+# Clean up
+rm /usr/bin/porcipine /usr/share/applications/porcupine.desktop /usr/share/pixmaps/porcupine.png /var/lib/oz/cells.d/porcupine.json /var/lib/oz/cells.d/porcupine-whitelist.seccomp
+
+if [ "$1" = "--subgraph" ]; then
+    # In Subgraph OS, install app and oz profile
+    sudo cp -r usr/* /usr/
+    mv /usr/bin/porcipine /usr/bin-oz/porcupine
+    ln -s /usr/bin/oz /usr/bin/porcupine
+    sudo cp -r var/* /var/
+
+    systemctl restart oz-daemon.service
+else
+    # Not in Subgraph OS, just install app
+    sudo cp -r usr/* /usr/
+fi
+
+# Set porcupine as the default browser
 xdg-settings set default-web-browser porcupine.desktop
